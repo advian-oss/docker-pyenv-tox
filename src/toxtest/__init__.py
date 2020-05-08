8c39acd7e1ec4b6428b8d60d83040818a1fdaa12
Eero af Heurlin <eero.afheurlin@iki.fi>
2020-05-08 10:15:21 +0300

    A bit more realistic tox env test

--- src/toxtest/__init__.py
+++ src/toxtest/__init__.py
@@ -1,1 +1,2 @@
 """Dummy package since tox needs something"""
+__version__ = "0.1.0"
---
+++ tests/test_toxtest.py
@@ -0,0 +1,7 @@
+"""Minimal test for testing test-runner"""
+from toxtest import __version__
+
+
+def test_version() -> None:
+    """Test the version string"""
+    assert __version__ == "0.1.0"
