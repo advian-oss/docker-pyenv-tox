6ce2fb68602fef11fbe2f525891da9d1a5e82713
Eero af Heurlin <eero.afheurlin@iki.fi>
2022-03-17 15:22:27 +0200

    Add support for non-semver versions
    and cargo fmt

--- pyenv_resolve/src/main.rs
+++ pyenv_resolve/src/main.rs
@@ -1,17 +1,21 @@
+use semver::Version;
 use shellfn::shell;
-use std::error::Error;
 use std::env;
-use semver::{Version};
+use std::error::Error;
 
 #[shell]
-fn pyenv_list_installed() -> Result<impl Iterator<Item=String>, Box<dyn Error>> { r#"
+fn pyenv_list_installed() -> Result<impl Iterator<Item = String>, Box<dyn Error>> {
+    r#"
     pyenv versions
-"# }
+"#
+}
 
 #[shell]
-fn pyenv_list_available() -> Result<impl Iterator<Item=String>, Box<dyn Error>> { r#"
+fn pyenv_list_available() -> Result<impl Iterator<Item = String>, Box<dyn Error>> {
+    r#"
     pyenv install -l
-"# }
+"#
+}
 
 fn pyenv_parse_semver(input: String) -> Result<Version, semver::Error> {
     let trimmed = input.trim().trim_start_matches("* ");
@@ -22,25 +26,30 @@ fn pyenv_parse_semver(input: String) -> Result<Version, semver::Error> {
 fn main() {
     // Figure out what versions to look for
     let args: Vec<String> = env::args().collect();
-    let mut resolved_versions: Vec<Version> = Vec::with_capacity(args.len()-1);
-    let mut look_for: Vec<Version> = Vec::with_capacity(args.len()-1);
+    let mut unresolved_requested_versions: Vec<String> = Vec::with_capacity(args.len() - 1);
+    let mut resolved_versions: Vec<Version> = Vec::with_capacity(args.len() - 1);
+    let mut look_for: Vec<Version> = Vec::with_capacity(args.len() - 1);
     for item in args {
-        if item.ends_with("pyenv_resolve") { continue };
-        match Version::parse(&item)   {
+        if item.ends_with("pyenv_resolve") {
+            continue;
+        };
+        match Version::parse(&item) {
             Err(_) => {
-                let vminor = item + ".1337715517-parsermarker";
+                let vminor = item.clone() + ".1337715517-parsermarker";
                 match Version::parse(&vminor) {
                     Ok(ver2) => look_for.push(ver2),
-                    Err(_) => continue,
+                    Err(_) => unresolved_requested_versions.push(item.clone()),
                 }
-            },
+            }
             // If the version was already fully defined just put it in the final vector
             Ok(ver) => resolved_versions.push(ver),
         };
     }
     look_for.sort();
     let look_for = look_for;
+    let unresolved_requested_versions = unresolved_requested_versions;
     // println!("look_for: {:?}", look_for);
+    // println!("unresolved_requested_versions: {:?}", unresolved_requested_versions);
 
     // Figure out which versions are installed
     let pyenv_inst = pyenv_list_installed().unwrap();
@@ -54,7 +63,6 @@ fn main() {
     installed_versions.sort();
     let installed_versions = installed_versions;
     // println!("installed_versions: {:?}", installed_versions);
-
 
     // Figure out which versions are available
     let pyenv_avail = pyenv_list_available().unwrap();
@@ -72,8 +80,12 @@ fn main() {
     for wanted_ver in look_for {
         let mut selected_ver: Version = wanted_ver.clone();
         for check_ver_inst in &installed_versions {
-            if check_ver_inst.major != wanted_ver.major { continue };
-            if check_ver_inst.minor != wanted_ver.minor { continue };
+            if check_ver_inst.major != wanted_ver.major {
+                continue;
+            };
+            if check_ver_inst.minor != wanted_ver.minor {
+                continue;
+            };
             selected_ver = check_ver_inst.clone();
         }
         // If we got a version from the installed go to next wanted
@@ -82,8 +94,12 @@ fn main() {
             continue;
         }
         for check_ver_avail in &available_versions {
-            if check_ver_avail.major != wanted_ver.major { continue };
-            if check_ver_avail.minor != wanted_ver.minor { continue };
+            if check_ver_avail.major != wanted_ver.major {
+                continue;
+            };
+            if check_ver_avail.minor != wanted_ver.minor {
+                continue;
+            };
             selected_ver = check_ver_avail.clone();
         }
         if selected_ver.patch == wanted_ver.patch && selected_ver.pre == wanted_ver.pre {
@@ -95,6 +111,9 @@ fn main() {
     //println!("resolved_versions: {:?}", resolved_versions);
     for ver in resolved_versions {
         print!("{} ", ver.to_string())
+    }
+    for ver in unresolved_requested_versions {
+        print!("{} ", ver)
     }
     println!("");
 }
