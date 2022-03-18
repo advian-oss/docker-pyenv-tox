6cf821824732c427ac88681413fbb7436933d1cd
Eero af Heurlin <eero.afheurlin@iki.fi>
2022-03-18 11:14:22 +0200

    different python versions need different libs

--- src/toxtest/__init__.py
+++ src/toxtest/__init__.py
@@ -1,2 +1,2 @@
 """Dummy package since tox needs something"""
-__version__ = "0.1.0"
+__version__ = "0.2.0"
--- tests/test_toxtest.py
+++ tests/test_toxtest.py
@@ -4,4 +4,4 @@ from toxtest import __version__
 
 def test_version() -> None:
     """Test the version string"""
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.2.0"
